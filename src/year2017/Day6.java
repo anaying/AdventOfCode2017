c65e13fbae35159621d3eb689be7b6275bbc3271
Ana Ying <anayingbl@gmail.com>
2025-11-04 11:40:56 +0100

    Día 6

--- src/year2017/Day6.java
+++ src/year2017/Day6.java
@@ -12,50 +12,13 @@ public class Day6 {
         }
         System.out.println("Solución 1: " + part1(array));
         System.out.println("Solución 2: " + part2(array));
-//        System.out.println();
-//        ArrayList<Integer> array2 = new ArrayList<>(List.of(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 2));
-//        System.out.println(array2.indexOf(Collections.max(array2)));
     }
 
-    /*
-        private static int part1(ArrayList<Integer> input) {
-            System.out.println(input.toString());
-    //        input = new ArrayList<>(List.of(0,2,7,0));
-            ArrayList<ArrayList<Integer>> list = new ArrayList<>();
-            list.add(input);
-            int max, index, sol = 0;
-            boolean repeated = false;
-            do {
-                ArrayList<Integer> temp = (ArrayList<Integer>) list.getLast().clone();
-                max = Collections.max(temp);
-                index = temp.indexOf(max);
-                for (int i = 0; i < input.size(); i++) {
-                    if (i != index)
-                        temp.set(i, temp.get(i) + max / (input.size() - 1));
-                    else
-                        temp.set(i, max % (input.size() - 1));
-                }
-                for (ArrayList<Integer> l : list) {
-                    if (l.equals(temp)) {
-                        repeated = true;
-                        break;
-                    }
-                }
-                System.out.println(temp);
-                sol++;
-                if (!repeated) {
-                    list.add(temp);
-                }
-            } while (!repeated);
-            return sol;
-        }
-    */
     private static int part1(ArrayList<Integer> input) {
 //        input = new ArrayList<>(List.of(0,2,7,0));
         ArrayList<ArrayList<Integer>> list = new ArrayList<>();
         list.add(input);
         int max, index, sol = 0;
-        boolean repeated = false;
         do {
             ArrayList<Integer> temp = (ArrayList<Integer>) list.getLast().clone();
             max = Collections.max(temp);
@@ -65,18 +28,14 @@ public class Day6 {
                 index = (index + 1) % input.size();
                 temp.set(index, temp.get(index) + 1);
             }
+            sol++;
             for (ArrayList<Integer> l : list) {
                 if (l.equals(temp)) {
-                    repeated = true;
-                    break;
+                    return sol;
                 }
             }
-            sol++;
-            if (!repeated) {
-                list.add(temp);
-            }
-        } while (!repeated);
-        return sol;
+            list.add(temp);
+        } while (true);
     }
 
     private static int part2(ArrayList<Integer> input) {
